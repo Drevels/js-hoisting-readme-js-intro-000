cc575c95a84dea24f1c9d89350837c1357ce1d2f
danielle revels <ddaniellerrevels@aol.com>
2020-07-22 02:44:57 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -7,3 +7,18 @@ function callMe() {
   return lyric;
 }
 
+function sayMyName() {
+//   // fix the code in here:
+ var name = "Cricky";
+   function sayMy() {
+   name = "Kristin"; // took out var before name here
+    console.log(name);
+  }
+ sayMy();
+ }
+ sayMyName()()
+
+var sayMyName = function() {
+// fix the code in here:
+  var name;
+  name = "Cricky";
